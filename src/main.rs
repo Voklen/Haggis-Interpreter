4225d67da9ac71949bdc4cd22d25b06d7243dcfe
Alex Gorichev <Alex.Gorichev@protonmail.com>
2022-06-09 21:36:49 +0100

    Added variables

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
 }
 
 fn run_haggis(contents: String) {
+	let mut vars = Variables::new();
 	for i in contents.lines() {
 		// Check if comment
 		let first_char = i.chars().next();
@@ -16,11 +17,16 @@ fn run_haggis(contents: String) {
 		// Run line
 		let words: Vec<&str> = i.split(' ').collect();
 		match words[0] {
-			"SEND" => send(i, words),
+			"SEND" => send(i, words, &vars),
+			"SET" => vars.set(i, words),
 			_ => println!("Unrecognized command: {}", words[0]),
 		}
 	}
 }
+
+/*
+INTERNAL FUNCTIONS
+*/
 
 fn get_file() -> String {
 	let file_location = "hello_world.hgs";
@@ -32,7 +38,8 @@ fn get_file() -> String {
 EXPRESSION EVALUATIONS
 */
 
-fn evaluate_as_str(input: &str) -> &str {
+
+fn evaluate_as_str<'a>(input: &'a str, vars: &'a Variables) -> String {
 	let quote = '"';
 
 	let first_char = input
@@ -43,30 +50,52 @@ fn evaluate_as_str(input: &str) -> &str {
 		.expect("Syntax error: Field cannot be empty");
 
 	if (first_char != quote) | (last_char != quote) {
-		return get_var_as_str(input);
+		return vars.get_str(input);
 	}
 
 	// At this point all checks have been passed and this is a string surrounded by quotes
 	// Remove first and last char (quotes)
-	let mut chars = input.chars();
-	chars.next();
-	chars.next_back();
-	chars.as_str()
+	input[1..input.len()-1].to_string()
 }
 
-/*
-VARIABLE GETTERS
-*/
+enum Types {
+	String(String),
+}
 
-fn get_var_as_str(input: &str) -> &str {
-	panic!("Syntax error: no variable {}", input);
+struct Variables {
+	vars: std::collections::HashMap<String, Types>,
+}
+impl Variables {
+	fn new() -> Variables {
+		use std::collections::HashMap;
+		let vars = HashMap::new();
+		Variables {vars}
+	}
+	fn set(&mut self, line: &str, words: Vec<&str>) {
+		const START_CHARS: usize = 8; // SET ... TO = 8 chars excluding the ...
+		if words.get(2) != Some(&"TO") {
+			panic!("Syntax Error: SEND must always have a TO")
+		}
+
+		let key = words[1].to_string();
+		let set_value = evaluate_as_str(&line[(START_CHARS + key.len())..], self);
+		let value = Types::String(set_value);
+		self.vars.insert(key, value);
+	}
+	fn get_str(&self, key: &str) -> String {
+		match self.vars.get(key) {
+			Some(Types::String(string)) => string.into(),
+			Some(_) => panic!("{key} is not a string"),
+			None => panic!("Syntax error: no variable {}", key)
+		}
+	}
 }
 
 /*
 BUILT IN FUNCTIONS
 */
 
-fn send(to_send: &str, words: Vec<&str>) {
+fn send(line: &str, words: Vec<&str>, vars: &Variables) {
 	// Check the second to last word is TO
 	let second_to_last_word = words.get(words.len().wrapping_sub(2));
 	if second_to_last_word != Some(&"TO") {
@@ -74,7 +103,7 @@ fn send(to_send: &str, words: Vec<&str>) {
 	}
 	let expr_end = second_to_last_word.unwrap().len() + words.last().unwrap().len() + 2; // .unwrap() should never panic as we have just checked that the second to last element exists (The +2 is for the spaces between the words)
 
-	let expression = &to_send[5..(to_send.len() - expr_end)]; // This get's everything between the SEND and the TO
-	let string = evaluate_as_str(expression);
+	let expression = &line[5..(line.len() - expr_end)]; // This get's everything between the SEND and the TO
+	let string = evaluate_as_str(expression, vars);
 	println!("{}", string);
 }
